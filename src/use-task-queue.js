fac03dfa3419396d9a2b20fb520ece72dd8a8152
Pedro Antoninho <pdn.antoninho@gmail.com>
2024-12-27 03:29:14 +0000

    cleanup+demo

--- src/use-task-queue.js
+++ src/use-task-queue.js
@@ -11,25 +11,42 @@ export function useTaskQueue({ threads = 5 } = {}) {
   const [activeThreads, setActiveThreads] = React.useState([]);
   /** @type {useState<Task[]>} */
   const [queue, setQueue] = React.useState([]);
+  /** @type {useState<TaskState[]>} */
+  const [tasks, setTasks] = React.useState([]);
+
+  function updateTaskState(id, state) {
+    setTasks((tasks) =>
+      tasks.map((t) => (t.id === id ? { ...t, ...state } : t)),
+    );
+  }
 
   React.useEffect(() => {
     if (activeThreads.length >= threads) return;
 
-    const job = queue.shift();
-    if (!job) return;
+    const task = queue.shift();
+    if (!task) return;
 
-    job
+    updateTaskState(task.id, { running: true });
+    task
       .run()
-      .then(job.onComplete)
-      .catch(job.onError)
-      .then(() =>
-        setActiveThreads((prev) => prev.filter((t) => t.id !== job.id)),
-      );
+      .then((data) => {
+        task.onComplete(data);
+        updateTaskState(task.id, { data });
+      })
+      .catch((error) => {
+        task.onError(error);
+        updateTaskState(task.id, { error });
+      })
+      .finally(() => {
+        updateTaskState(task.id, { running: false, complete: true });
+        setActiveThreads((prev) => prev.filter((t) => t.id !== task.id));
+      });
 
-    setActiveThreads((prev) => [...prev, job]);
+    setActiveThreads((prev) => [...prev, task]);
   }, [queue, activeThreads, threads]);
 
   return {
+    tasks,
     /**
      * adds a task to the queue
      * @param {Function} fn function to be executed
@@ -37,14 +54,22 @@ export function useTaskQueue({ threads = 5 } = {}) {
      */
     add: async (fn) => {
       return new Promise((resolve, reject) => {
-        const job = {
+        const task = {
           id: crypto.randomUUID(),
           run: fn,
           onComplete: resolve,
           onError: reject,
         };
 
-        setQueue((prev) => [...prev, job]);
+        const taskState = {
+          id: task.id,
+          running: false,
+          data: null,
+          error: null,
+        };
+
+        setTasks((prev) => [...prev, taskState]);
+        setQueue((prev) => [...prev, task]);
       });
     },
   };
@@ -53,14 +78,24 @@ export function useTaskQueue({ threads = 5 } = {}) {
 /**
  * @typedef {Object} TaskQueue
  * @property {Function} add
+ * @property {TaskState[]} tasks
  */
 
 /**
  * @typedef {Object} Task
  * @property {string} id
- * @property {Function} run
+ * @property {() => Promise<unknown>} run
  * @property {Function} onComplete
  * @property {Function} onError
+ */
+
+/**
+ * @typedef {Object} TaskState
+ * @property {string} id
+ * @property {boolean} running
+ * @property {boolean} complete
+ * @property {unknown} data
+ * @property {unknown} error
  */
 
 /**
